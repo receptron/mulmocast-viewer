83161f27fd4c14c6ac7594ba4a979e97f0d99b09
isamu <isamu@to-kyo.to>
2025-10-12 16:37:30 +0900

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import eslint from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["dist", "node_modules", "**/*.js"]
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    files: ["src/**/*.{vue,ts}"],
+    plugins: {
+      "typescript-eslint": tseslint.plugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/no-unused-vars": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+];
